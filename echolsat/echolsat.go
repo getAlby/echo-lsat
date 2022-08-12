27ae2b554b72fc569cee27248ef02f04d6056e67
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2022-08-12 15:32:17 +0530

    Remove unused env loading

--- echolsat/echolsat.go
+++ echolsat/echolsat.go
@@ -2,7 +2,6 @@ package echolsat
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -13,7 +12,6 @@ import (
 	macaroonutils "github.com/getAlby/echo-lsat/macaroon"
 	"github.com/getAlby/echo-lsat/utils"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -62,13 +60,7 @@ func NewLsatMiddleware(lnClientConfig *ln.LNClientConfig,
 	return middleware, nil
 }
 
-func InitLnClient(lnClientConfig *ln.LNClientConfig) (ln.LNClient, error) {
-	var lnClient ln.LNClient
-	err := godotenv.Load(".env")
-	if err != nil {
-		return lnClient, errors.New("Failed to load .env file")
-	}
-
+func InitLnClient(lnClientConfig *ln.LNClientConfig) (lnClient ln.LNClient, err error) {
 	switch lnClientConfig.LNClientType {
 	case LND_CLIENT_TYPE:
 		lnClient, err = ln.NewLNDclient(lnClientConfig.LNDConfig)
